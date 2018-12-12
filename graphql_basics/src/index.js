dc87ec40c38be4ef7abf0c2a3d017b2e3fcde344
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-11 19:46:54 -0700

    added mutations for createUser createPost and createComments

--- graphql_basics/src/index.js
+++ graphql_basics/src/index.js
@@ -1,11 +1,13 @@
 import { GraphQLServer } from 'graphql-yoga';
 import faker from 'faker';
 
+// Generate data with faker
 const usersArr = [];
 const userIds = [];
 const postIds = [];
 const postsArr = [];
 const commentsArr = [];
+// generate users
 for (let i = 0; i < 10; i++) {
   let user = {
     id: faker.random.uuid(),
@@ -16,6 +18,7 @@ for (let i = 0; i < 10; i++) {
   userIds.push(user.id);
   usersArr.push(user);
 }
+// generate posts
 for (let i = 0; i < 50; i++) {
   let post = {
     id: faker.random.uuid(),
@@ -27,7 +30,7 @@ for (let i = 0; i < 50; i++) {
   postIds.push(post.id);
   postsArr.push(post);
 }
-
+// generate commnets
 for (let i = 0; i < 100; i++) {
   let comment = {
     id: faker.random.uuid(),
@@ -47,6 +50,11 @@ type Query{
   user:User!
   post:Post!
   comment:Comment!
+},
+type Mutation{
+  createUser(name:String!,email:String!,age:Int!):User!
+  createPost(title:String!,body:String!,published:Boolean!,author:ID!):Post!
+   createComment(text:String!,author:ID!,post:ID!):Comment!
 }
 type User{
   id:ID!
@@ -115,6 +123,55 @@ const resolvers = {
         body: 'Some body',
         published: true
       };
+    }
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const { name, email, age } = args;
+
+      const emailTaken = usersArr.some(user => {
+        return user.email === email;
+      });
+      if (emailTaken) {
+        throw new Error('Email taken.');
+      }
+      let newUser = { id: faker.random.uuid(), name, email, age };
+      usersArr.push(newUser);
+      return newUser;
+    },
+    createPost(parent, args, ctx, info) {
+      const { title, body, published, author } = args;
+      const userExists = usersArr.some(user => user.id === author);
+      if (!userExists) {
+        throw new Error('No user found');
+      }
+      let newPost = {
+        id: faker.random.uuid(),
+        title,
+        body,
+        published,
+        author
+      };
+      postsArr.push(newPost);
+      return newPost;
+    },
+    createComment(parent, args, ctx, info) {
+      const { text, author, post } = args;
+      const postExists = postsArr.some(
+        somePost => somePost.id === post && somePost.published
+      );
+      const userExists = usersArr.some(user => user.id === author);
+
+      if (!postExists || !userExists) {
+        throw new Error('Invalid post or user');
+      }
+      let newComment = {
+        id: faker.random.uuid(),
+        text,
+        author,
+        post
+      };
+      return newComment;
     }
   },
 
